516289870666564549edca51b1ffe1222d2e78e8
Cameron Messinides <cameron.messinides@gmail.com>
2025-01-26 22:19:21 -0500

    Add Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { resolve } from 'path'
-import kirby from 'vite-plugin-kirby'
+import { resolve } from "path";
+import kirby from "vite-plugin-kirby";
+import tailwindcss from "@tailwindcss/vite";
 
 export default ({ mode }) => ({
-  root: 'src',
-  base: mode === 'development' ? '/' : '/dist/',
+  root: "src",
+  base: mode === "development" ? "/" : "/dist/",
 
   build: {
-    outDir: resolve(process.cwd(), 'public/dist'),
+    outDir: resolve(process.cwd(), "public/dist"),
     emptyOutDir: true,
-    rollupOptions: { input: resolve(process.cwd(), 'src/index.js') }
+    rollupOptions: { input: resolve(process.cwd(), "src/index.js") },
   },
 
-  plugins: [kirby()]
-})
+  plugins: [kirby(), tailwindcss()],
+});
+
